fe98c86c24e1f6f9db04a05cb82afcab8ec1f9dc
redashu <ashutoshh@linux.com>
2018-08-10 22:09:59 +0530

    day one with ML

---
+++ task_np.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+comment='''
+Press  1  to  ask matrix dimension from user and elements values :-->
+Press  2  to ask from user to give some number of input the print there possible              matrix  :-->
+Press  3  to  save all your numpy array data into  file :--->
+Press  4  to print all your file data into  numpy format :--->
+Press  5  to apply all possible reshape functions :--->
+
+'''
+
+print(comment)
